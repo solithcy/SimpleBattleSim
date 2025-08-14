990be1d47bfddbc49d982ea6be1650ca6858b26c
solithcy <me@rose.gay>
2025-08-14 11:42:32 +0100

    add other character tests

--- Testing/CharacterTests.cs
+++ Testing/CharacterTests.cs
@@ -20,4 +20,22 @@ public class Tests
         }
         Assert.That(healths.Max(), Is.GreaterThan(10));
     }
+
+    [Test]
+    public void Check_That_WizardHealth_Decrements_AfterAttack()
+    {
+        var w = new Wizard(null);
+        var health = w.Health;
+        w.Attack();
+        Assert.That(w.Health, Is.EqualTo(health-1));
+    }
+
+    [Test]
+    public void Check_That_ClericHealth_Increments_AfterAttack()
+    {
+        var c = new Cleric(null);
+        var health = c.Health;
+        c.Attack();
+        Assert.That(c.Health, Is.EqualTo(health+1));
+    }
 }
